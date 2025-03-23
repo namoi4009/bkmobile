7cd942cee09190e791e6636fe99511fb84b1b744
lethanhbinhxq <binh.lethanh@hcmut.edu.vn>
2025-03-23 21:56:21 +0700

    Add username input field

--- FlowerApp/app/src/main/java/com/example/flowerapp/MainActivity.kt
+++ FlowerApp/app/src/main/java/com/example/flowerapp/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.flowerapp
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.flowerapp.ui.theme.FlowerAppTheme
@@ -19,9 +34,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FlowerAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +47,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars)
+    ) {
+        NormalTextField()
+    }
+}
+
+@Composable
+fun NormalTextField() {
+    var textInput by remember {
+        mutableStateOf("")
+    }
+    TextField(
+        value = textInput,
+        onValueChange = {
+            newValue -> textInput = newValue
+        },
+        label = { Text("Username") },
+        placeholder = { Text("Enter your username") },
+        leadingIcon = { Icon(Icons.Default.Person, contentDescription = "username") },
+        trailingIcon = {
+            IconButton(onClick = {
+                textInput = ""
+            }) {
+                Icon(Icons.Default.Clear, contentDescription = "clear username")
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     FlowerAppTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
