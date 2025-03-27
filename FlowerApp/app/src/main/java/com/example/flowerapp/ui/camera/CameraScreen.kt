b0cd905d475feeed3868d8a0356964b9ab1a00e4
lethanhbinhxq <binh.lethanh@hcmut.edu.vn>
2025-03-27 09:05:17 +0700

    Init camera module

---
+++ FlowerApp/app/src/main/java/com/example/flowerapp/ui/camera/CameraScreen.kt
@@ -0,0 +1,9 @@
+package com.example.flowerapp.ui.camera
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CameraScreen() {
+    Text("Hello camera")
+}
